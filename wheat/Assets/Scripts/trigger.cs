5191d5ef96f133c35ff33aae99db14cb18e579f8
dotoby <dotoby@gmail.com>
2020-02-02 15:13:42 -0500

    changed some map stuff

---
+++ wheat/Assets/Scripts/trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+class trigger : MonoBehaviour
+{
+    public Flowchart flowchart;
+    private CoolCameraManager cameraMan;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        flowchart.ExecuteBlock("Tractor");
+    }
+}
