8be287d3b534f1c1f9ce40d3ebc0dceeeaa64ec7
vars416 <56270900+vars416@users.noreply.github.com>
2020-02-02 12:24:34 -0500

    Main Menu scene and script added

---
+++ wheat/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
